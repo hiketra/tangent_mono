8212d06d380b82dec5f724efd64f9591fd2c8588
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2017-08-09 09:49:26 +0100

    database retrieval working

--- index.js
+++ index.js
@@ -4,16 +4,29 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var api = require('./neo4jApi');
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req,res){
+  api.getChildMessagesForNode(353).then(msgs => {
+    console.log("about to remder page" + msgs)
+    debugger;
+    console.log("messages:" + msgs)
+    res.render('index.ejs', {messages: msgs})
+  }).catch(error=> {
+    console.log(error)
+  })
 });
+
+// app.get('/', function(req, res){
+//   res.sendFile(__dirname + '/index.html');
+// });
 
 io.on('connection', function(socket){
   //console.log("I'm in the index.js" + api.getChildMessagesForNode(353))
-  api.getChildMessagesForNode(353).then(message => {
-    if(!message) return;
-    io.emit('chat message', message);
-  })
+  // api.getChildMessagesForNode(353).then(messages => {
+  //   if(!message) return;
+  //   io.emit('retrieved_messages', messages);
+  // })
   //io.emit('retrieved_messages', api.getChildMessagesForNode(353))
   //io.emit('messages', api.getMessages)
 
