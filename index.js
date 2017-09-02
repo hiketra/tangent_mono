8640834fc8d27245f7da0d3c69bcdc2d4d9a21ab
Emma-Louise Upton <emma-louise.upton@hmrcaspire.com>
2017-09-02 14:58:58 +0100

    beautified

--- index.js
+++ index.js
@@ -8,38 +8,44 @@ var _ = require('lodash');
 
 app.set('view engine', 'ejs');
 
-app.get('/', function(req,res){
-  api.getChildMessagesForNode(375).then(msgs => {
-    debugger;
-    res.render('index.ejs', {messages: msgs.reverse(), relations: [{identity:"212"}, {identity:"2211"}]})
-  }).catch(error=> {
-    console.log(error)
-  })
+app.get('/', function(req, res) {
+    api.getChildMessagesForNode(375).then(msgs => {
+        api.getNodeTree(370).then(tree => {
+            res.render('index.ejs', {
+                messages: msgs.reverse(),
+                nodes: tree
+            })
+        })
+    }).catch(error => {
+        console.log(error)
+    })
 });
 
 // app.get('/', function(req, res){
 //   res.sendFile(__dirname + '/index.html');
 // });
 
-io.on('connection', function(socket){
-  //console.log("I'm in the index.js" + api.getChildMessagesForNode(353))
-  // api.getChildMessagesForNode(353).then(messages => {
-  //   if(!message) return;
-  //   io.emit('retrieved_messages', messages);
-  // })
-  //io.emit('retrieved_messages', api.getChildMessagesForNode(353))
-  //io.emit('messages', api.getMessages)
+io.on('connection', function(socket) {
+    //console.log("I'm in the index.js" + api.getChildMessagesForNode(353))
+    // api.getChildMessagesForNode(353).then(messages => {
+    //   if(!message) return;
+    //   io.emit('retrieved_messages', messages);
+    // })
+    //io.emit('retrieved_messages', api.getChildMessagesForNode(353))
+    //io.emit('messages', api.getMessages)
 
-  socket.on('chat message', function(messageBundle){
-    console.log(`received message:${messageBundle}`)
-    api.getNodeTree(370)
-    api.makeMessageParentAndCreateChild(messageBundle.parentNode, messageBundle.message).then(msg=>{
-    io.emit('chat message', msg)}
-  ).catch(error => {console.log(error)})
-      //api.getMessages();
-  });
+    socket.on('chat message', function(messageBundle) {
+        console.log(`received message:${messageBundle}`)
+        api.getNodeTree(370)
+        api.makeMessageParentAndCreateChild(messageBundle.parentNode, messageBundle.message).then(msg => {
+            io.emit('chat message', msg)
+        }).catch(error => {
+            console.log(error)
+        })
+        //api.getMessages();
+    });
 });
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
+http.listen(port, function() {
+    console.log('listening on *:' + port);
 });
