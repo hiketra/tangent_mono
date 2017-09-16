1865da3b76aee299b66288a416c67b88f7e92eee
Emma <emma.upton.95@gmail.com>
2017-09-16 16:05:24 +0100

    channel creation and additional channel info end points done

--- index.js
+++ index.js
@@ -4,53 +4,74 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 var api = require('./neo4jApi');
 var _ = require('lodash');
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    api.getChildMessagesForNode(370).then(msgs => {
-        api.getNodeTree(370).then(tree => {
-            res.render('index.ejs', {
-                messages: msgs.reverse(),
-                nodes: tree
-            })
-        })
-    }).catch(error => {
-        console.log(error)
-    })
+	api.getChildMessagesForNode(370).then(msgs => {
+		api.getNodeTree(370).then(tree => {
+			res.render('index.ejs', {
+				messages: msgs.reverse(),
+				nodes: tree
+			})
+		})
+	}).catch(error => {
+		console.log(error)
+	})
 });
 
 app.get('/chat/:nodeId', function(req, res) {
-  let nodeId = req.params.nodeId;
-  api.getChildMessagesForNode(nodeId).then(msgs => {
-    api.getNodeTree(nodeId).then(tree => {
-      res.render('index.ejs', {
-        messages: msgs.reverse(),
-        nodes: tree
-      })
-    })
-  })
+	let nodeId = req.params.nodeId;
+  api.getChannelInfoById(nodeId).then( channelInfo => {
+	api.getChildMessagesForNode(nodeId).then(msgs => {
+		api.getNodeTree(channelInfo).then(tree => {
+      debugger;
+			res.render('index.ejs', {
+				messages: msgs.reverse(),
+				nodes: tree
+			})
+		})
+	})})
 });
 
-api.get('/healthcheck', function(req, res) {
-  res.sendStatus(200);
+app.get('/create-channel', function(req, res) {
+	res.render('createNewChannel.ejs', {})
+})
+
+app.post('/channel-creation-complete', function(req, res) {
+	api.createNewChannel(req.body.channel.name).then(channel => {
+		console.log(channel)
+		res.render('channelCreationPage.ejs', {
+			identity: channel.channelId
+		})
+	})
+})
+
+app.get('/healthcheck', function(req, res) {
+	res.sendStatus(200);
 })
 
 
 io.on('connection', function(socket) {
 
-    socket.on('chat message', function(messageBundle) {
-        console.log(`received message:${messageBundle}`)
-        api.getNodeTree(370)
-        api.makeMessageParentAndCreateChild(messageBundle.parentNode, messageBundle.message).then(msg => {
-            io.emit('chat message', msg)
-        }).catch(error => {
-            console.log(error)
-        })
-    });
+	socket.on('chat message', function(messageBundle) {
+		console.log(`received message:${messageBundle}`)
+		api.getNodeTree(370)
+		api.makeMessageParentAndCreateChild(messageBundle.parentNode, messageBundle.message).then(msg => {
+			io.emit('chat message', msg)
+		}).catch(error => {
+			console.log(error)
+		})
+	});
 });
 
 http.listen(port, function() {
-    console.log('listening on *:' + port);
+	console.log('listening on *:' + port);
 });
--- neo4jApi.js
+++ neo4jApi.js
@@ -3,38 +3,40 @@ const FlatToNested = require('flat-to-nested');
 const driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neo4j"));
 
 function extractPropertiesAndNodeId(properties, identity) {
-  return {
-    //TODO: Account for low and high math/big ints, just do a search for identity.low
-    identity: identity.low,
-    //this would be a lot easier if I could get the damn babel spread operator working
-    message: properties.message,
-    timeSeconds: properties.timeSeconds,
-    timeMinutes: properties.timeMinitues,
-    timeHours: properties.timeHours,
-    timeDay: properties.timeDay,
-    timeMonth: properties.timeMonth,
-    timeYear: properties.timeYear
-  }
+	return {
+		//TODO: Account for low and high math/big ints, just do a search for identity.low
+		identity: identity.low,
+		//this would be a lot easier if I could get the damn babel spread operator working
+		message: properties.message,
+		timeSeconds: properties.timeSeconds,
+		timeMinutes: properties.timeMinitues,
+		timeHours: properties.timeHours,
+		timeDay: properties.timeDay,
+		timeMonth: properties.timeMonth,
+		timeYear: properties.timeYear
+	}
 }
 
 //No discrimination between a child or channel
 function getChildMessagesForNode(nodeId) {
-  var session = driver.session();
-  console.log(`Obtaining messages for ${nodeId}`)
-  return resultPromise = session.run(
-    `START n=node(${nodeId}) MATCH (n)-[:IS_PARENT_OF]->(m) return m`
-  ).then(result => {
-    session.close();
-    mapped = result.records.map(record => extractPropertiesAndNodeId(record.get(0).properties, record.get(0).identity))
-    return mapped
-  })
-  .catch(error => {
-    session.close();
-    throw error;
-  })
+	var session = driver.session();
+	console.log(`Obtaining messages for ${nodeId}`)
+	return resultPromise = session.run(
+			`START n=node(${nodeId}) MATCH (n)-[:IS_PARENT_OF]->(m) return m`
+		).then(result => {
+			session.close();
+			mapped = result.records.map(record => extractPropertiesAndNodeId(record.get(0).properties, record.get(0).identity))
+			return mapped
+		})
+		.catch(error => {
+			session.close();
+			throw error;
+		})
 }
 
-function getNodeTree(nodeId) {
+function getNodeTree(channelInfo) {
+  var nodeId = channelInfo.channelId
+  var name = channelInfo.channelName
   var session = driver.session();
   console.log(`Obtaining messages for ${nodeId}`)
   return resultPromise = session.run(
@@ -60,7 +62,8 @@ function getNodeTree(nodeId) {
     let extracted = results.records.map(result => extraction(result.get(0)))
     //TODO: Make root node child
     //TODO: get actual node and message
-    extracted.push({id: nodeId, message: "channel name"})
+    extracted.push({id: nodeId, message: name})
+    debugger;
     flatToNested = new FlatToNested();
     let flattened = flatToNested.convert(extracted)
     console.log("flattened:" + flattened)
@@ -73,61 +76,75 @@ function getNodeTree(nodeId) {
 }
 
 function makeMessageParentAndCreateChild(nodeId, childMessage) {
-  //TODO: Update relationship on nodeId, make it child
-  //TODO: Graphical updates to frontend - message has blue dot, tree updated/re-rendered
-  var session = driver.session();
-  console.log(`Making message ${nodeId} have child ${childMessage}`)
-  return resultPromise = session.run(
-    `START n=node(${nodeId}) SET n.isParent=TRUE CREATE (m:Message{message:'${childMessage}', timeMilliseconds: '${new Date().getMilliseconds()}', timeSeconds: '${new Date().getSeconds()}', timeMinitues: '${new Date().getMinutes()}', timeHours: '${new Date().getHours()}', timeDay: '${new Date().getDate()}', timeMonth: '${new Date().getMonth()}', timeYear: '${new Date().getFullYear()}'}) CREATE (n)-[r:IS_PARENT_OF]->(m) RETURN m`
-  ).then(result => {
-    session.close()
-    let x = extractPropertiesAndNodeId(result.records[0].get(0).properties, result.records[0].get(0).identity)
-    console.log("message bundle:" + JSON.stringify(x, null, 2))
-    return x
-  })
-  .catch(error => {
-      session.close();
-      throw error;
-    })
+	//TODO: Update relationship on nodeId, make it child
+	//TODO: Graphical updates to frontend - message has blue dot, tree updated/re-rendered
+	var session = driver.session();
+	console.log(`Making message ${nodeId} have child ${childMessage}`)
+	return resultPromise = session.run(
+			`START n=node(${nodeId}) SET n.isParent=TRUE CREATE (m:Message{message:'${childMessage}', timeMilliseconds: '${new Date().getMilliseconds()}', timeSeconds: '${new Date().getSeconds()}', timeMinitues: '${new Date().getMinutes()}', timeHours: '${new Date().getHours()}', timeDay: '${new Date().getDate()}', timeMonth: '${new Date().getMonth()}', timeYear: '${new Date().getFullYear()}'}) CREATE (n)-[r:IS_PARENT_OF]->(m) RETURN m`
+		).then(result => {
+			session.close()
+			let x = extractPropertiesAndNodeId(result.records[0].get(0).properties, result.records[0].get(0).identity)
+			console.log("message bundle:" + JSON.stringify(x, null, 2))
+			return x
+		})
+		.catch(error => {
+			session.close();
+			throw error;
+		})
 }
 
 function getChannelInfoById(identifier) {
-  var session = driver.session();
-  return resultPromise = session.run(
-    `MATCH (c:Channel WHERE c.name=${name} RETURN c)`
-  ).then(result => {
-    session.close()
-    let x = {channelName: result.records[0].get(0).properties.name,
-            channelId: result.records[0].get(0).identity}
-    return x;
-  })
-  .catch(error => {
-    session.close();
-    throw error;
-  })
+	var session = driver.session();
+	return resultPromise = session.run(
+			`START n=node(${identifier}) return n`
+		).then(result => {
+			session.close()
+      debugger;
+      var name = ''
+      //TODO: update to label check
+      if(result.records[0].get(0).properties.message) {
+        name = result.records[0].get(0).properties.message
+      }
+      else {
+        name = result.records[0].get(0).properties.name
+      }
+      let x = {
+        channelName: name,
+        channelId: result.records[0].get(0).identity.low
+      }
+			return x;
+		})
+		.catch(error => {
+			session.close();
+			throw error;
+		})
 }
 
 function createNewChannel(channelName) {
-  //TODO: add in error handling for when two channels created of same name
-  var session = driver.session();
-  return resultPromise = session.run(
-    `CREATE (c:Channel {isParent: true, name: ${channelName}}) RETURN c`
-  ).then(result => {
-    session.close()
-    let x = {channelName: result.records[0].get(0).properties.name,
-            channelId: result.records[0].get(0).identity}
-    return x;
-  })
-  .catch(error => {
-    session.close();
-    throw error;
-  })
+	//TODO: add in error handling for when two channels created of same name
+	var session = driver.session();
+	return resultPromise = session.run(
+			`CREATE (c:Channel {isParent: true, name: "${channelName}"}) RETURN c`
+		).then(result => {
+			session.close()
+			let x = {
+				channelName: result.records[0].get(0).properties.name,
+				channelId: result.records[0].get(0).identity.low
+			}
+			return x;
+		})
+		.catch(error => {
+			session.close();
+			throw error;
+		})
 }
 
 function getParentOfNode() {
-  //TODO: Unimplemented
+	//TODO: Unimplemented
 }
-
+exports.getChannelInfoById = getChannelInfoById
+exports.createNewChannel = createNewChannel
 exports.getChildMessagesForNode = getChildMessagesForNode
 exports.getNodeTree = getNodeTree
 exports.makeMessageParentAndCreateChild = makeMessageParentAndCreateChild
